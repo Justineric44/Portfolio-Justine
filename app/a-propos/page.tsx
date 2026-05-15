3d9a8f17c4db7d5cd02e0858235f85849164998d
Justineric44 <justine.robard@hotmail.fr>
2026-05-15 14:55:24 +0200

    Fix use client a-propos

--- app/a-propos/page.tsx
+++ app/a-propos/page.tsx
@@ -1,13 +1,11 @@
+"use client"
 
-import Apropos from "@/components/a-propos";
 import dynamic from 'next/dynamic'
 
 const APropos = dynamic(() => import('@/components/a-propos'), { ssr: false })
 
 export default function AproposPage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center p-24 gap-4">
-      <Apropos />
-    </main>
-  );
+    <APropos />
+  )
 }
